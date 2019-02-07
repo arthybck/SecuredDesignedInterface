f5cd9d0c72d4188b13408e9b37c98d374d976927
dtixador <dany.tixador@gmail.com>
2019-02-07 02:09:07 +0100

    instantiate fragments page + design

--- app/src/main/java/fr/tixado_d/secureddesignedinterface/Fragments/SettingsFragment.java
+++ app/src/main/java/fr/tixado_d/secureddesignedinterface/Fragments/SettingsFragment.java
@@ -1,6 +1,17 @@
 package fr.tixado_d.secureddesignedinterface.Fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.tixado_d.secureddesignedinterface.R;
 
 public class SettingsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+
 }
