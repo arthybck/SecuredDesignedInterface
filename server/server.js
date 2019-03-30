f0c3152e4b8a35f4ad7f3b01d09bbc58e6e20d91
Arthur Brunck <arthur.brunck@neoxia.com>
2019-03-30 20:27:24 +0100

    Format switch and logger used

--- server/server.js
+++ server/server.js
@@ -6,36 +6,36 @@
  * @Last modified time: 2019-02-07T10:08:31+01:00
  */
 
-"use strict";
+'use strict';
 
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const helmet = require("helmet");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const helmet = require('helmet');
 
-const authController = require("./controllers/auth.js");
-const userController = require("./controllers/users.js");
+const authController = require('./controllers/auth.js');
+const userController = require('./controllers/users.js');
+
+const logBuilder = require('./helpers/logger.js');
+
 //TODO seed to apply for the delivery
 // const seed = require("./seed.js
 
-const port = process.env.BUILD_ENVIRONMENT === "PRODUCTION" ? 3000 : 3080;
+const port = process.env.BUILD_ENVIRONMENT === 'PRODUCTION' ? 3000 : 3080;
 const mongooseUri =
-  process.env.BUILD_ENVIRONMENT === "PRODUCTION"
-    ? "mongodb://localhost:27017/arthybck"
-    : "mongodb://localhost:27017/arthybck-dev";
+  process.env.BUILD_ENVIRONMENT === 'PRODUCTION'
+    ? 'mongodb://localhost:27017/arthybck'
+    : 'mongodb://localhost:27017/arthybck-dev';
 
 initMongoConnection();
 function initMongoConnection() {
-  console.time("[*] Booting");
+  console.time('[*] Booting');
   mongoose.Promise = global.Promise;
-  mongoose.connect(
-    mongooseUri,
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true
-    }
-  );
+  mongoose.connect(mongooseUri, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  });
 }
 
 const app = express();
@@ -44,41 +44,49 @@ configApp(app);
 function routerConfiguration(app) {
   const router = express.Router();
 
-  router.route("/").get((req, res) => {
-    return res.status(200).send("Hello World!");
+  router.route('/').get((req, res) => {
+    return res.status(200).send('Hello World!');
   });
 
   // ------------------------ USER CONTROLLER ------------------------
 
   //Register    --- POST
-  router.route("/register").post(userController.registerUser);
+  router
+    .route('/register')
+    .post(logBuilder.myLogger, userController.registerUser);
 
   //Login       --- POST
-  router.route("/login").post(userController.logUser);
+  router.route('/login').post(logBuilder.myLogger, userController.logUser);
 
   //Profile     --- GET
   router
-    .route("/profile")
-    .get(authController.isAuthenticated, userController.getUser);
+    .route('/profile')
+    .get(
+      logBuilder.myLogger,
+      authController.isAuthenticated,
+      userController.getUser
+    );
 
   //Profile     --- GET PUT
   router
-    .route("/users")
+    .route('/users')
     .get(
+      logBuilder.myLogger,
       authController.isAuthenticated,
       authController.isMembers,
       userController.getUsers
     )
     .put(
+      logBuilder.myLogger,
       authController.isAuthenticated,
       authController.isMembers,
       userController.putUser
     );
 
   //Profile     --- GET
-  router.route("/logout").get((req, res) => {
+  router.route('/logout').get(logBuilder.myLogger, (req, res) => {
     console.log(
-      passport.authenticate("jwt", {
+      passport.authenticate('jwt', {
         session: false
       })
     );
@@ -103,15 +111,15 @@ function configApp(app) {
   app.use(bodyParser.json());
   app.use(passport.initialize());
   app.use((req, res, forward) => {
-    res.header("Content-Security-Policy", "default-src 'self'");
-    res.header("X-Frame-Options", "SAMEORIGIN");
-    res.header("X-XSS-Protection", "1; mode=block");
-    res.header("X-Content-Type-Options", "nosniff");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header('Content-Security-Policy', "default-src 'self'");
+    res.header('X-Frame-Options', 'SAMEORIGIN');
+    res.header('X-XSS-Protection', '1; mode=block');
+    res.header('X-Content-Type-Options', 'nosniff');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Authorization, x-access-token, Accept"
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Authorization, x-access-token, Accept'
     );
     forward();
   });
@@ -120,16 +128,16 @@ function configApp(app) {
 
 const server = app.listen(port, () => {
   console.log(
-    "[*] REST API listening at: %s:%s",
+    '[*] REST API listening at: %s:%s',
     server.address().address,
     server.address().port
   );
-  console.log("[*] Mongoose URI: %s", mongooseUri);
+  console.log('[*] Mongoose URI: %s', mongooseUri);
 
-  console.log("\n\n\r");
+  console.log('\n\n\r');
 
-  process.on("SIGINT", () => {
-    console.log("\n\nServer shutting down  !\n\r");
+  process.on('SIGINT', () => {
+    console.log('\n\nServer shutting down  !\n\r');
     process.exit(0);
   });
 });
